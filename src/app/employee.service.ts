74dda9ee2865934a8b1d92e9bc2744626d77a8b6
Jimmy Lo <83715466+JimmyCHL@users.noreply.github.com>
2025-02-03 09:10:37 -0500

    Update employee.service.ts
    do not have domain name, so could not use ingress_nginx yml file intergration in the kubernetes cluster for proxy routing, so directly change 8080 to 30008 because 30008 is the port that is exposed to the world.

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -8,7 +8,7 @@ import { Employee } from './employee';
 })
 export class EmployeeService {
 
-  private baseUrl = "http://18.226.200.21:8080/api/v1/employees";
+  private baseUrl = "http://18.226.200.21:30008/api/v1/employees";
   constructor(private httpclient: HttpClient) { }
 
   getEmployeesList(): Observable<Employee[]>{
